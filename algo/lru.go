050aac106fdf5d238814805f23a4824d5c141df2
dreambbsd <xmldn123@outlook.com>
2022-12-26 11:50:20 +0800

    lru算法

---
+++ algo/lru.go
@@ -0,0 +1,56 @@
+package algo
+
+import "container/list"
+
+type Cache struct {
+	maxBytes  int64
+	nbytes    int64
+	ll        *list.List
+	cache     map[string]*list.Element
+	onEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	len() int
+}
+
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		onEvicted: onEvicted,
+	}
+}
+
+// Get look ups a key`s value
+func (c *Cache) Get(key string) (Value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// Add adds a value to the cache.
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.len()) - int64(kv.value.len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.len())
+	}
+	//for c.maxBytes !=0 && c.maxBytes < c.nbytes  {
+	//	c.RemoveOldest()
+	//}
+}
