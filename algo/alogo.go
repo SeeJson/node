961dea04116b4d0f609045d6e19bb9b8063aa14a
dreambbsd <xmldn123@outlook.com>
2023-03-14 22:21:35 +0800

    lru算法

---
+++ algo/alogo.go
@@ -0,0 +1,156 @@
+package algo
+
+import (
+	"bufio"
+	"encoding/gob"
+	"io"
+	"sort"
+)
+
+// 三数之和
+func threeSum(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	ans := make([][]int, 0)
+
+	// 枚举 a
+	for first := 0; first < n; first++ {
+		// 需要和上一次枚举的数不相同
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		// c 对应的指针初始指向数组的最右端
+		third := n - 1
+		target := -1 * nums[first]
+		// 枚举 b
+		for second := first + 1; second < n; second++ {
+			// 需要和上一次枚举的数不相同
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+			// 需要保证 b 的指针在 c 的指针的左侧
+			for second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			// 如果指针重合，随着 b 后续的增加
+			// 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+			if second == third {
+				break
+			}
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
+	}
+	return ans
+}
+
+/**
+ * Definition for singly-linked list.
+ *
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getLength(head *ListNode) (length int) {
+	for ; head != nil; head = head.Next {
+		length++
+	}
+	return
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	length := getLength(head)
+	dummy := &ListNode{0, head}
+	cur := dummy
+	for i := 0; i < length-n; i++ {
+		cur = cur.Next
+	}
+	cur.Next = cur.Next.Next
+	return dummy.Next
+}
+
+type GobCodec struct {
+	conn io.ReadWriteCloser
+	buf  *bufio.Writer
+	dec  *gob.Decoder
+	enc  *gob.Encoder
+}
+
+/*
+旋转图像
+*/
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	// 水平翻转
+	for i := 0; i < n/2; i++ {
+		matrix[i], matrix[n-1-i] = matrix[n-1-i], matrix[i]
+	}
+	// 主对角线翻转
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}
+
+/*
+pow(x, n)
+求 x的n次方
+复杂度分析
+	时间复杂度：�(log⁡�)*O*(log*n*)，即为递归的层数。
+空间复杂度：�(log⁡�)*O*(log*n*)，即为递归的层数。这是由于递归的函数调用会使用栈空间。
+*/
+func quickMul(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	y := quickMul(x, n/2)
+	if n%2 == 0 {
+		return y * y
+	}
+	return y * y * x
+}
+func quickMul_good(x float64, N int) float64 {
+	ans := 1.0
+	// 贡献的初始值为 x
+	x_contribute := x
+	// 在对 N 进行二进制拆分的同时计算答案
+	for N > 0 {
+		if N%2 == 1 {
+			// 如果 N 二进制表示的最低位为 1，那么需要计入贡献
+			ans *= x_contribute
+		}
+		// 将贡献不断地平方
+		x_contribute *= x_contribute
+		// 舍弃 N 二进制表示的最低位，这样我们每次只要判断最低位即可
+		N /= 2
+	}
+	return ans
+}
+
+func myPow(x float64, n int) float64 {
+	if n >= 0 {
+		return quickMul(x, n)
+	}
+	return 1.0 / quickMul(x, n)
+}
+
+/*
+求最大子数组的和
+*/
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
---
+++ algo/alogo_test.go
@@ -0,0 +1,109 @@
+package algo
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCache(t *testing.T) {
+
+	println("xx")
+}
+
+type Stu struct {
+}
+
+func TestCache1(t *testing.T) {
+	q := make(chan int, 2)
+	q <- 1
+	q <- 2
+	select {
+	case q <- 3:
+		fmt.Println("ok")
+	default:
+		for i := 0; i < 3; i++ {
+			c := <-q
+			fmt.Println(c)
+		}
+
+		fmt.Println("wrong")
+	}
+}
+
+func Test_rotate(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	ins := []int{1, 2, 3, 3, 24, 2, 7, 8, 4, 3, 2, 5455, 343, 65464, 54}
+	mate := [][]int{ins, ins, ins}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{name: "xxx", args: args{matrix: mate}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rotate(tt.args.matrix)
+			t.Log(tt.args.matrix)
+		})
+	}
+}
+
+func Test_quickMul(t *testing.T) {
+	type args struct {
+		x float64
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+		{args: args{x: 2, n: 3}, want: 8},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := quickMul(tt.args.x, tt.args.n); got != tt.want {
+				t.Errorf("quickMul() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func cew(x float64, n int) float64 {
+	ans := 1.0
+	arr_ans := x
+	for n > 0 {
+		if n%2 == 1 {
+			ans *= x * arr_ans
+		}
+		arr_ans *= arr_ans
+		n /= 2
+	}
+	return ans
+}
+
+func Test_maxSubArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	nums := []int{-2, 1, -3, 4}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{want: 6, args: args{nums: nums}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxSubArray(tt.args.nums); got != tt.want {
+				t.Errorf("maxSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
